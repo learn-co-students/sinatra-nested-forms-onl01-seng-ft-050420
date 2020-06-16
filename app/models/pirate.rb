63d34e15900bb560982501cb9a6f97d8353471f6
uharston <uharston@gmail.com>
2020-06-16 16:42:23 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,23 @@
+require 'pry'
 class Pirate
+
+    attr_accessor :name, :weight, :height 
+
+    @@all = [] 
+
+    def initialize(hash)
+        # @name = name 
+        # @weight = weight 
+        # @height = height
+
+        @name = hash[:name]
+        @weight = hash[:weight]
+        @height = hash[:height]
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
+require 'pry'
 class Ship
+
+    attr_accessor :name, :type, :booty 
+
+    @@all_ships = [] 
+
+    def initialize(hash)
+        @name = hash[:name] 
+        @type = hash[:type] 
+        @booty = hash[:booty]
+        @@all_ships << self 
+        
+    end 
+
+    def self.all 
+        @@all_ships 
+    end 
+
+    def self.clear 
+        @@all_ships.clear 
+    end 
+
 end
