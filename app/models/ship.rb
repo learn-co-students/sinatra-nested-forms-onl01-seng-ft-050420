e3a3228c1c4645d1a445da7be95aec347209518b
Kamrin <kamrinkennedy@gmail.com>
2020-06-16 17:58:04 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(attr_hash = {})
+        attr_hash.each {|k,v| self.send("#{k}=", v)}
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end
