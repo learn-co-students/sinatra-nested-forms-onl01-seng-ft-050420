a233ea6895a1aa1f9e32daff92516cc8632f0237
anette87 <anettem.rivera@gmail.com>
2020-06-17 10:08:44 -0700

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,11 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    def initialize(arguments)
+        @name = arguments[:name]
+        @weight = arguments[:weight] 
+        @height = arguments[:height] 
+    end 
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+
+    def initialize(arguments)
+        @name = arguments[:name]
+        @type = arguments[:type] 
+        @booty = arguments[:booty]  
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def self.clear
+        @@all.clear
+    end 
+    
 end
